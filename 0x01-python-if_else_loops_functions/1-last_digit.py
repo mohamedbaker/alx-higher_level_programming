8bdcdd73d1aad97c312df435f9bb909e3b565a3f
mohamedbaker <dam.vampire22@gmail.com>
2023-10-31 18:06:08 -0700

    word count

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number > 10:
     last_digit = number % 10
 else:
     last_digit = number % -10
-print("Last digit of", number, "is", last_digit, " and is ", end="")
+print("Last digit of", number, "is", last_digit, "and is ", end="")
 if last_digit > 5:
     print("greater than 5")
 elif last_digit == 0:
