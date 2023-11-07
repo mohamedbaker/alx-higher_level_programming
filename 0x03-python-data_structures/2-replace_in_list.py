cc239e1d008650736c6f05d3ee1e61a72f3b635a
mohamedbaker <dam.vampire22@gmail.com>
2023-11-07 02:39:00 +0200

    a

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that replaces an element of a list by idx
+If idx is negative, the function should return None.
+f idx is out of range, the function should return None.
+"""
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return
+    else:
+        my_list[idx] = element
