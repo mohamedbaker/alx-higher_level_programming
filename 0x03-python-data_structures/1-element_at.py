5b2f3faf6ba025cfa71962eabe7db89eb579d7b2
mohamedbaker <dam.vampire22@gmail.com>
2023-11-07 02:23:38 +0200

    a

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -7,7 +7,7 @@ f idx is out of range, the function should return None.
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return
     else:
         return my_list[idx]
