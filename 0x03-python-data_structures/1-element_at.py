c41bd0119dcce3955d10d8792958628083515e5d
mohamedbaker <dam.vampire22@gmail.com>
2023-11-07 02:17:19 +0200

    a

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,10 @@
 """
 function that retrieves an element from a list like in C using index.
 If idx is negative, the function should return None.
-f idx is out of range (> of number of element in my_list), the function should return None.
+f idx is out of range, the function should return None.
 """
+
+
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return
