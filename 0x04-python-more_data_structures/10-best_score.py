87de15ca08c3ed965bff757aecb0301fe64638ee
mohamedbaker <dam.vampire22@gmail.com>
2023-11-09 04:38:51 +0200

    a

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return
+        return None
     max_value = 0
     new_key = None
     for key, value in a_dictionary.items():
