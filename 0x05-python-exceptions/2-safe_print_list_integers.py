c5c6a32a7066eb18c934ecac41332daed7e435f3
mohamedbaker <dam.vampire22@gmail.com>
2023-11-20 19:15:51 +0200

    a

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    z = 0
+    y = 0
+    while z < x:
+        try:
+            print("{:d}".format(my_list[z]), end="")
+            y += 1
+        except (ValueError, TypeError):
+            pass
+        z += 1
+    print()
+    return z
