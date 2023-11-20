a967a43db5a5302c28075b40da743495ca33cccb
mohamedbaker <dam.vampire22@gmail.com>
2023-11-20 14:16:46 +0200

    a

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0){
+def safe_print_list(my_list=[], x=0)
 	int indx = 0
 
 	try:
@@ -11,4 +11,3 @@ def safe_print_list(my_list=[], x=0){
 		None
 	print()
 	return indx
-}
