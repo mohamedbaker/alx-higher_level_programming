ab99c683bb5905c5047b394c4329903a7350d8ed
mohamedbaker <dam.vampire22@gmail.com>
2023-11-21 21:58:22 +0200

    a

---
+++ 1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''sqaure module'''
+
+
+class Square:
+    '''init square'''
+
+    def __init__(self, size):
+        '''constractor
+
+         Args:
+             size: size of a square side.
+        '''
+        self.__size = size
