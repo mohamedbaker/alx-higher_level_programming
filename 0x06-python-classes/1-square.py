df99026ce22954aee04f6f3284c3ac714ec31926
mohamedbaker <dam.vampire22@gmail.com>
2023-11-21 22:09:56 +0200

    a

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""sqaure module."""
+
+
+class Square:
+    """init square
+       attr:
+            __size: size of square
+    """
+
+    def __init__(self, size):
+        """constractor.
+
+         Args:
+             size: size of a square side.
+        """
+        self.__size = size
