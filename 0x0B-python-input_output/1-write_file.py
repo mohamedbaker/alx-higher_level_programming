0d2503fdf6d1b250a06394afb45cc56d21ec42c8
mohamedbaker <dam.vampire22@gmail.com>
2023-12-06 01:57:54 +0200

    a

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,5 +8,5 @@ def write_file(filename="", text=""):
          return: the number of characters written
     """
     with open(filename, 'w', encoding="UTF-8") as f:
-        return(f.write(text))
+        return (f.write(text))
         f.close()
