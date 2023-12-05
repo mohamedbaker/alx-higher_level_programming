002792c8ade49652122f73232d406cc3f433b115
mohamedbaker <dam.vampire22@gmail.com>
2023-12-05 03:05:34 +0200

    a

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' module checking if obj is instance for same class '''
+
+
+def is_kind_of_class(obj, a_class):
+    ''' method checks if obj is instance for same class
+        Return : true if obj is instnce otherwise false.
+    '''
+
+    return isinstance(obj, a_class)
