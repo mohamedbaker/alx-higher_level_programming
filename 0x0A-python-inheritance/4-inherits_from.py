5dea464e84dfcafc4f8e19752806cbaf8c0794e9
mohamedbaker <dam.vampire22@gmail.com>
2023-12-05 03:07:19 +0200

    a

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' module checking if obj is instance for same class or not'
+
+
+def inherits_from(obj, a_class):
+    ''' method checks if obj is instance for same class
+        Return : true if obj is instnce otherwise false.
+    '''
+
+    return isinstance(obj, a_class)
