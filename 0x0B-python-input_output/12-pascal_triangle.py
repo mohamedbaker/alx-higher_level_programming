56aaac662ae7d7c2dce92f3001f3ef766f4498f3
mohamedbaker <dam.vampire22@gmail.com>
2023-12-06 03:23:06 +0200

    a

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a Pascal's triangle function."""
+
+
+def pascal_rowangle(n):
+    """Represent Pascal's tringle with size n.
+
+       Return:  list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    rows = [[1]]
+    while len(rows) != n:
+        row = rows[-1]
+        tmp = [1]
+        for i in range(len(row) - 1):
+            tmp.append(row[i] + row[i + 1])
+        tmp.append(1)
+        rows.append(tmp)
+    return rows
