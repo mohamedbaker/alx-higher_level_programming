fe1b8fe427e9e53c298c9d9008a3c3de9eda4d00
mohamedbaker <dam.vampire22@gmail.com>
2023-12-06 02:07:41 +0200

    a

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module returns the JSON repres."""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+        func to_json_strin
+        return: JSON representation
+    """
+    return json.dumps(my_obj)
