62d6c65aac4810b94679d5f2e060615e121812e8
mohamedbaker <dam.vampire22@gmail.com>
2023-12-11 23:36:25 +0200

    a

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bn/python3
+'''Module for sqaure cls. '''
+from models.rectangle import Rectangle
+
+
+class sqaure(Recatngle):
+    '''sqaure cls extends from Rectangle.'''
+    def __init__(self, size, x=0, y=0, id=None):
+        ''' constructor '''
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        ''' overrid str and return str foramt for sqr'''
+        return ("[{}] ({}) {}/{} - {}".format(type(self).__name__,
+                                              self.id, self.x, self.y,
+                                              self.width))
