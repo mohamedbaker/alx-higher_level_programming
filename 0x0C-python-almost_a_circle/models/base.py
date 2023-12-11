7a747d2b7e36d7e2605d67be06c812c3f7a00883
mohamedbaker <dam.vampire22@gmail.com>
2023-12-12 00:33:08 +0200

    a

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,7 @@
 '''class will be the “base” of all other classes in this project.
      The goal of it is to manage id attribute in all classes and
       to avoid duplicating the same code'''
-
+from json import dumps, loads
 
 class Base:
     ''' bass class to to avoid dublication.'''
@@ -15,3 +15,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
