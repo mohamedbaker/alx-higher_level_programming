1e290bb423488b5a93222f60b89185c9ccca008e
mohamedbaker <dam.vampire22@gmail.com>
2023-12-11 21:16:31 +0200

    a

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,7 @@
 from models.base import Base
 
 
-class Rectanglei(Base):
+class Rectangle(Base):
     ''' Rectangle class inhertiting from Base class'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -11,7 +11,7 @@ class Rectanglei(Base):
 
         super().__init__(id)
         self.__width = width
-        self._height = height
+        self.__height = height
         self.__x = x
         self.__y = y
 
@@ -21,6 +21,10 @@ class Rectanglei(Base):
 
     @width.setter
     def width(self, new_width):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         return self.__width
 
     @property
@@ -29,6 +33,10 @@ class Rectanglei(Base):
 
     @height.setter
     def height(self, new_height):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         return self.__height
 
     @property
@@ -37,6 +45,10 @@ class Rectanglei(Base):
 
     @x.setter
     def x(self, new_x):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         return self.__x
 
     @property
@@ -45,4 +57,30 @@ class Rectanglei(Base):
 
     @y.setter
     def y(self, new_y):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         return self.__y
+
+    def area(self):
+        '''
+            returns the area of the Rectangle.
+        '''
+        return (self.__width * self.__height)
+
+    def display(self):
+
+        for v in range(self.y):
+            print("")
+        for r in range(self.height):
+            print(" " * self.x, end="")
+            print("#" * self.width)
+
+    def __str__(self):
+        '''
+            returns a string format
+        '''
+        return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, self.id,
+                                                self.__x, self.__y,
+                                                self.__width, self.__height)
