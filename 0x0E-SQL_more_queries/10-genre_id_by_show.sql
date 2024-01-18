1b175310c5fe86b42227edeaebccbc8accaca6d8
mohamedbaker <dam.vampire22@gmail.com>
2024-01-18 04:14:32 +0200

    a

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+--  script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+RIGHT JOIN tv_show_genres
+ON tv_shows.genre_id = tv_show_genres.genre_id
+ORDER BY tv_shows.id ASC, tv_show_genres.genre_id ASC;
