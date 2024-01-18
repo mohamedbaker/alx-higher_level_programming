9f85aa960e8a61ba40530457353a364f91734f63
mohamedbaker <dam.vampire22@gmail.com>
2024-01-18 03:24:00 +0200

    ''

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 -- should have all privileges with password user_0d_1_pwd
 
 CREATE USER IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED BY user_0d_1_pwd;
+IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
 ON *.*
 TO user_0d_1@localhost;
