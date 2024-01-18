2bd7318cafcc3330993589e88ceda7a644ceb626
mohamedbaker <dam.vampire22@gmail.com>
2024-01-18 03:08:05 +0200

    a

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- script that creates the MySQL server user user_0d_1
+-- should have all privileges with password user_0d_1_pwd
+
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL
+ON *.*
+TO user_0d_1@localhost;
