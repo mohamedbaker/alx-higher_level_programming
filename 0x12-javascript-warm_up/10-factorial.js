a8518b74b46388a0799ec2cec376d36dca86bc86
mohamedbaker <dam.vampire22@gmail.com>
2024-02-13 01:29:06 +0200

    a

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (n < 0) {
+    return (-1);
+  }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+const x = Number(process.argv[2]);
+console.log(factorial(x));
