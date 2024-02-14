b20e95fae070e28b05fbdcdea9109388b4a89ac5
mohamedbaker <dam.vampire22@gmail.com>
2024-02-14 02:37:45 +0200

    rotate and double methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+/*
+* Rectangle: class that defines rectangle.
+* Description: Create an instance method called rotate().
+* exchanges the width and the height.
+* Create an instance method called double().
+* that multiples the width and the height of the rectangle by 2.
+*/
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+
+  rotate () {
+    let temp = 0;
+    temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
