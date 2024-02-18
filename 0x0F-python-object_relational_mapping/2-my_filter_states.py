5a220cf82204899da4cfa8926fa8a82be9d3b85c
mohamedbaker <dam.vampire22@gmail.com>
2024-02-19 01:57:57 +0200

    list where name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+a script that lists all states starts with
+from the database 'hbtn_0e_0_usa'.
+'''
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    '''
+    connect to db and get the states data.
+    '''
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""
+             SELECT * FROM states
+             WHERE name = '{}'
+             ORDER BY states.id ASC
+                """.format(argv[4]))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
