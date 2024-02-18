d0bf75b2b513117e1ad9daede8292c68388b2505
mohamedbaker <dam.vampire22@gmail.com>
2024-02-19 00:30:59 +0200

    a

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ from the database.
 """
 
 if __name__ == "__main__":
-  db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY ID ASC")
