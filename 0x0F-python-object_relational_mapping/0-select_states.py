2b89538bbf284e7e2d79a487876719494a6ae6ea
mohamedbaker <dam.vampire22@gmail.com>
2024-02-19 00:39:56 +0200

    comm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
+
 import MySQLdb
 from sys import argv
-"""
+
+'''
 a script that lists all states
 from the database.
-"""
+'''
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
