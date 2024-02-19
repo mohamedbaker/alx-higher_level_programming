07431080a70cc041616045a0f5069f311beef60b
mohamedbaker <dam.vampire22@gmail.com>
2024-02-19 02:19:48 +0200

    pycodestyle

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
              SELECT * FROM states
              WHERE name LIKE %s
              ORDER BY states.id ASC
-                """,(argv[4],))
+                """, (argv[4],))
     rows = cur.fetchall()
 
     for row in rows:
