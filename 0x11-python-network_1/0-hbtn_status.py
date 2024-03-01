2bd38cfd93570c5b12e05dc45e505677bdb2e561
mohamedbaker <dam.vampire22@gmail.com>
2024-03-02 00:53:26 +0200

    s

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
    with urlopen("https://alx-intranet.hbtn.io/status") as response:
         body = response.read()
-        print(f"""Body response:
-\t- type: {type(body)}
-\t- content: {body}
-\t- utf8 content: {body.decode('utf-8')} """)
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
